d272e2fb0b802a12b57bf2c069e5d2efb8d7c9d3
joshCurrie <jmojcurrie@gmail.com>
2020-08-12 09:20:56 -0400

    Social media changes, introducing new social stuff in header and changed header pic

---
+++ src/Components/SocialNetworks.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import styled from 'styled-components'
+
+const SocialNetworks = ({ NetworkData, Color }) => {
+
+    const customColoredA = styled.a`
+        color: ${Color};
+    `
+
+    if (Color) {
+        var style = "color: " + Color + ";";
+    }
+
+    if (NetworkData) {
+        var networks = NetworkData.map(function(network){
+            return <li key={network.name}><a Style={style} href={network.url}><i className={network.className}></i></a></li>
+        });
+    }
+
+    return <div>{networks}</div>
+}
+
+SocialNetworks.propTypes = {
+    /*
+     * NetworkData: From resumecontent.json, an array of content about my social networks
+     */
+    NetworkData: PropTypes.array,
+    /*
+     * Color: Color of the icons 
+     */
+    Color: PropTypes.string,
+  };
+
+
+export default SocialNetworks
